0bec556f57b3d0b9509cd01576a8917bf42104a2
Travis Smith <mellendeveloper@gmail.com>
2022-11-05 13:33:23 -0400

    add subnav

---
+++ src/NavBar/SubNav/SubNav.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { SubNavItem } from './SubNavItem/SubNavItem';
+import styles from './SubNav.module.css';
+
+export function SubNav() {
+    return (
+        <div className={styles.container}>
+            <div className={styles['sub-nav']}>
+                <div>
+                    <SubNavItem label='Restaurant' showRightBorder />
+                </div>
+                <div>
+                    <button className={`button ${styles['subnav-button']} ${styles['omit-right-border']}`}>
+                        <span className="icon">
+                            <i className="fas fa-pen"></i>
+                        </span>
+                        <span>Write a Review</span>
+                    </button>
+                    <button className={`button ${styles['subnav-button']} ${styles['omit-right-border']}`}>
+                        <span className="icon">
+                            <i className="fas fa-hotel"></i>
+                        </span>
+                        <span>Businesses</span>
+                    </button>
+                </div>
+
+
+            </div>
+        </div>
+
+    );
+}
