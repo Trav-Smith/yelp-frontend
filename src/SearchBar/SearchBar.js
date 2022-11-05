6b1aa43e1210c3a80913c2e5b48520ee392fecfe
Travis Smith <mellendeveloper@gmail.com>
2022-11-04 20:38:30 -0400

    add NavBar

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import styles from './SearchBar.module.css';
 
-export function SearchBar() {
+export function SearchBar(props) {
+    const sizeClass = props.small ? '' : 'is-medium';
     return (
         <div>
             <div className="field has-addons">
 
                 <p className="control">
-                    <button className="button is-static is-medium">
+                    <button className={`button is-static ${sizeClass}`}>
                         Search
                     </button>
                 </p>
                 <p className="control">
-                    <input className={`input is-medium ${styles['input-control']}`} type="text" placeholder="Restaurants" />
+                    <input className={`input ${sizeClass} ${styles['input-control']}`} type="text" placeholder="Restaurants" />
                 </p>
                 <p className="control">
-                    <button className="button is-static is-medium">
+                    <button className={`button is-static ${sizeClass}`}>
                         Location
                     </button>
                 </p>
                 <p className="control">
-                    <input className={`input is-medium ${styles['input-control']}`} type="text" placeholder="Boiling Springs" />
+                    <input className={`input ${sizeClass} ${styles['input-control']}`} type="text" placeholder="Boiling Springs" />
                 </p>
-                <button className={`button is-medium ${styles['search-button']}`}>
+                <button className={`button ${sizeClass} ${styles['search-button']}`}>
                     <span className={`icon is-small ${styles['search-icon']}`}>
                         <i className="fas fa-search"></i>
                     </span>
